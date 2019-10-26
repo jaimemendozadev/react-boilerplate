d99e40c4eff8082aefc1f91cdfe51e2a0a8fae77
jaimemendozadev <jaimemendozadev@gmail.com>
2019-10-26 11:42:35 -0700

    Update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const devConfig = require('./webpack/dev.config');
 const baseConfig = require('./webpack/base.conifg');
+
 module.exports = env => {
 
   if(env.dev) {
